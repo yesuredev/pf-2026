131323b0a18d6b22f38e3d1cfe92eb7d3818db36
juhan <yesuredev@gmail.com>
2026-03-12 18:08:01 +0900

    scss/components/pages 구조 셋팅, scss 컴파일러

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+// import "./index.css";
+import "./styles/abstracts/_variables.scss";
+import "./styles/abstracts/_mixins.scss";
+import "./styles/base/reset.scss";
+import "./styles/main.scss";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
